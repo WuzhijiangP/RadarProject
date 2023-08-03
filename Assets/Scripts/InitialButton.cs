487490379d5678b8225233660084b3ecb5583698
HatsuneMiku <2178777037@qq.com>
2023-08-03 10:44:16 +0800

    雷达项目更新v1.0

--- Assets/Scripts/InitialButton.cs
+++ Assets/Scripts/InitialButton.cs
@@ -4,34 +4,96 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 
+[SerializeField]
+public enum GameScene { Plane, Radar, Scene, }
 public class InitialButton : MonoBehaviour
 {
     public List<Button> ButtonList;
     int index = 0;  //按钮计数器
 
+    public GameScene gamescene;
+
     // Start is called before the first frame update
     void Start()
     {
-        index = 0;
-        foreach (KeyValuePair<string, Sprite> kvp in CreateItemController.buttonInfo)
+        switch (gamescene)
         {
-            string key = kvp.Key;
-            Sprite value = kvp.Value;
-
-            //判断标记位类型
-            if (key.Split("+")[1].Equals("Plane"))
-            {
-                //激活一个按钮
-                if (!ButtonList[index].gameObject.activeInHierarchy)
+            case GameScene.Plane:
+                index = 0;
+                foreach (KeyValuePair<string, Sprite> kvp in CreateItemController.buttonInfo)
                 {
-                    ButtonList[index].gameObject.SetActive(true);
+                    string key = kvp.Key;
+                    Sprite value = kvp.Value;
+
+                    //判断标记位类型
+                    if (key.Split("+")[1].Equals("Plane"))
+                    {
+                        //激活一个按钮
+                        if (!ButtonList[index].gameObject.activeInHierarchy)
+                        {
+                            ButtonList[index].gameObject.SetActive(true);
+                            Debug.Log("当前下标：" + index);
+                        }
+                        //动态生成按钮图片
+                        ButtonList[index].GetComponentInChildren<TMP_Text>().text = key.Split("+")[0];
+                        ButtonList[index].GetComponentInChildren<Image>().sprite = value;
+                        index++;
+                    }
+
                 }
-                //动态生成按钮图片
-                ButtonList[index].GetComponentInChildren<TMP_Text>().text = key.Split("+")[0];
-                ButtonList[index].GetComponentInChildren<Image>().sprite = value;
-            }
-            index++;
+                index = 0;
+                break;
+            case GameScene.Radar:
+                index = 0;
+                foreach (KeyValuePair<string, Sprite> kvp in CreateItemController.buttonInfo)
+                {
+                    string key = kvp.Key;
+                    Sprite value = kvp.Value;
+
+                    //判断标记位类型
+                    if (key.Split("+")[1].Equals("Radar"))
+                    {
+                        //激活一个按钮
+                        if (!ButtonList[index].gameObject.activeInHierarchy)
+                        {
+                            ButtonList[index].gameObject.SetActive(true);
+                            Debug.Log("当前下标：" + index);
+                        }
+                        //动态生成按钮图片
+                        ButtonList[index].GetComponentInChildren<TMP_Text>().text = key.Split("+")[0];
+                        ButtonList[index].GetComponentInChildren<Image>().sprite = value;
+                        index++;
+                    }
+                }
+                index = 0;
+                break;
+            case GameScene.Scene:
+                index = 0;
+                foreach (KeyValuePair<string, Sprite> kvp in CreateItemController.buttonInfo)
+                {
+                    string key = kvp.Key;
+                    Sprite value = kvp.Value;
+
+                    //判断标记位类型
+                    if (key.Split("+")[1].Equals("Scene"))
+                    {
+                        //激活一个按钮
+                        if (!ButtonList[index].gameObject.activeInHierarchy)
+                        {
+                            ButtonList[index].gameObject.SetActive(true);
+                            Debug.Log("当前下标：" + index);
+                        }
+                        //动态生成按钮图片
+                        ButtonList[index].GetComponentInChildren<TMP_Text>().text = key.Split("+")[0];
+                        ButtonList[index].GetComponentInChildren<Image>().sprite = value;
+                        index++;
+                    }
+                }
+                index = 0;
+                break;
         }
+
+
     }
 
     // Update is called once per frame
